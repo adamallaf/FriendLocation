a5a05186a9e9bcef6d7c88d4cb58fd6c870066c7
Adam Allaf <adam.allaf@gigaset.com>
2017-08-07 10:06:02 +0200

    Updated win setup script

--- backend/setup_env/CreateVirtualEnv.bat
+++ backend/setup_env/CreateVirtualEnv.bat
@@ -3,6 +3,7 @@
 @set PYPATH=C:\Python36
 @set ACTIVATION_SCRIPT=ActivateVirtualEnv.bat
 @set DEACTIVATION_SCRIPT=DeactivateVirtualEnv.bat
+@set RUN_UT_SCRIPT=RunUnitTests.bat
 
 @set SQLURL=https://dev.mysql.com/get/Downloads/Connector-Python/
 @set SQLCONNECTOR=mysql-connector-python-2.1.6.zip
@@ -10,20 +11,26 @@
 @set VIRTUAL_ENV=friendLocation
 
 @cd ..
-%PYTHON% -m pip install setuptools --upgrade
-%PYTHON% -m pip install virtualenv --upgrade
+@%PYTHON% -m pip install setuptools --upgrade
+@%PYTHON% -m pip install virtualenv --upgrade
 @mkdir .pyEnv & cd .pyEnv
-%PYTHON% -m virtualenv --python=%PYPATH%\%PYTHON%.exe %VIRTUAL_ENV%
+@%PYTHON% -m virtualenv --python=%PYPATH%\%PYTHON%.exe %VIRTUAL_ENV%
 @cd ..
-@echo call ".pyEnv\%VIRTUAL_ENV%\Scripts\activate"> %ACTIVATION_SCRIPT% & echo.>> %ACTIVATION_SCRIPT%
-@echo deactivate> %DEACTIVATION_SCRIPT%
+@echo @call ".pyEnv\%VIRTUAL_ENV%\Scripts\activate.bat"> %ACTIVATION_SCRIPT%
+:: & echo.>> %ACTIVATION_SCRIPT%
+@echo @deactivate> %DEACTIVATION_SCRIPT%
 @call %ACTIVATION_SCRIPT%
 @cd setup_env
-pip install -r requirements.txt --upgrade
+@pip install -r requirements.txt --upgrade
 @cd ..
 
 :: Download and install mysql-connector-python from mysql.com
 bitsadmin.exe /transfer "downloading mysql-connector-python" %SQLURL%%SQLCONNECTOR% %cd%\%SQLCONNECTOR%
 ::python setup.py install
 
-@call RunUnitTests.bat
+@call %RUN_UT_SCRIPT%
+@endlocal
+
+@cd ..
+
+@call ActivateVirtualEnv.bat
